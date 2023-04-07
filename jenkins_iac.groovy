d96cbb18a45b16782a27e15325b7d8b599f20516
popowiec <grzegorz.wiktorowski@gmail.com>
2023-04-07 11:48:42 +0200

    first step

--- jenkins_iac.groovy
+++ jenkins_iac.groovy
@@ -1,1 +1,16 @@
-println "This is message from DSL job groovy script file!"
+import groovy.json.JsonSlurper
+
+//configuration root path
+def config_root_path = "${WORKSPACE}/jenkins_job"
+
+//linux tree command
+def tree_command = "tree -J ${config_root_path}"
+
+// get root tree of jobs config in json format
+def getJsonConfigTree(command) {
+    def tree = ['bash', '-c', "${command}"].execute().text
+    return new JsonSlurper().parseText(tree)
+}
+
+
+println getJsonConfigTree(tree_command)
