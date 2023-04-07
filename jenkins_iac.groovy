d6259c2ccd552825c2ff486d19a2b34cc80d9f74
popowiec <grzegorz.wiktorowski@gmail.com>
2023-04-07 12:33:07 +0200

    first step

--- jenkins_iac.groovy
+++ jenkins_iac.groovy
@@ -14,4 +14,38 @@ def getJsonConfigTree(command) {
 
 def jsonProjectsStruct = getJsonConfigTree(tree_command)
 
+for (jsonProjectStruct in jsonProjectsStruct[0].contents) {
+    jsonProjectStruct.info = jsonProjectStruct.name + " builds"
+    
+    println dslBuildProject(jsonProjectStruct, [])
+
+    // println dslBuildView(jsonProjectStruct)
+}
+
+
+//recursive function to build nested folder and job structure
+def dslBuildProject(jsonProjectStruct, rootFolderPath) {
+    def localFolderPath = rootFolderPath.clone()
+    localFolderPath.add(jsonProjectStruct.name)    
+
+    // build folders structure
+    if (jsonProjectStruct.type == "directory") {
+        dslBuildFolder(localFolderPath.join('/'), jsonProjectStruct)
+        for (content in jsonProjectStruct.contents) {
+            content.info = content.name
+            dslBuildProject(content, localFolderPath)
+        }
+    }
+
+    // build jobs
+    if (jsonProjectStruct.type == "file") {
+        def jobName = jsonProjectStruct.name =~ /(.*)\.jenkinsfile/
+        dslBuildJob(rootFolderPath.join('/'), jobName[0][1])
+    }
+
+    return "Completion of project building: ${localFolderPath.join('/')}"
+}
+
+
+
 println jsonProjectsStruct
